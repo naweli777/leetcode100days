c8cdef7cf375a558f26ba919ef6b04a8988ff5ac
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-05-28 19:07:14 +0530

    Time: 24 ms (83.29%), Space: 19.1 MB (83.65%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,22 +2,42 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-        //Extra space solution//
-      vector<int>temp;
-        for(int i=0; i<nums.size(); i++)
+        
+        int i=0; 
+        int j=1;
+        
+        while(j <nums.size())
         {
-            if(nums[i] !=0)
+            if((nums[i]==0 && nums[j] !=0))
             {
-              temp.push_back(nums[i]);
+                swap(nums[i],nums[j]);
             }
-          
+            else if((nums[i] ==0 && nums[j]==0))
+            {
+                j++;
+                continue;
+            }
+            i++;
+            j++;
         }
         
-        for(int i=temp.size(); i<nums.size(); i++)
-        {
-            temp.push_back(0);
-        }
-        nums = temp;
+        
+        //Extra space solution//
+//       vector<int>temp;
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             if(nums[i] !=0)
+//             {
+//               temp.push_back(nums[i]);
+//             }
+          
+//         }
+        
+//         for(int i=temp.size(); i<nums.size(); i++)
+//         {
+//             temp.push_back(0);
+//         }
+//         nums = temp;
         //Extra space solution ends//
         
  
