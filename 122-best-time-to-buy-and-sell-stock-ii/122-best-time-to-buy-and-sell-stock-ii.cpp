76c3283448190af03acdd2b598fdbc6deb825b16
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-05-31 10:50:24 +0530

    Time: 9 ms (56.44%), Space: 13 MB (88.49%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len=prices.size();
+        int ans=0;
+        for(int i=1;i<len;i++)
+        {
+            if(prices[i]>prices[i-1])
+            {
+           ans+=prices[i]-prices[i-1];
+            }
+        }
+        
+        return ans;
+    }
+};
