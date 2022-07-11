3542826c5b9558847a33f84b8454f28317f4faa6
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-07-11 09:15:40 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.22%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,26 +2,43 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
          int n = matrix.size();
-        vector<vector<int>>dummy(n, vector < int > (n, 0));
+        // vector<vector<int>>dummy(n, vector < int > (n, 0));
         
        
         
+//         for(int i=0; i<n; i++)
+//         {
+//             for(int j=0; j<n; j++)
+//             {
+//                 dummy[j][n-i-1] = matrix[i][j];
+                
+//             }
+//         }
+        
+//         for(int i=0; i<n; i++)
+//         {
+//             for(int j=0; j<n; j++)
+//             {
+//                 matrix[i][j] = dummy[i][j];
+//             }
+//         }
+        
+//  -----------------Brute force sol ends here-------------------
+        
         for(int i=0; i<n; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j=0; j<i; j++)
             {
-                dummy[j][n-i-1] = matrix[i][j];
-                
+                swap(matrix[i][j],matrix[j][i]);
             }
+            
         }
         
         for(int i=0; i<n; i++)
         {
-            for(int j=0; j<n; j++)
-            {
-                matrix[i][j] = dummy[i][j];
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
+        
         
     }
 };
