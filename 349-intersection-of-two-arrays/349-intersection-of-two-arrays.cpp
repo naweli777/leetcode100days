c0db5c662fac3c43a20191a5f0d1809bf4c0822c
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-07-23 15:51:39 +0530

    Time: 7 ms (76.50%), Space: 10.1 MB (78.97%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,30 +1,33 @@
-class Solution
-{
-  public:
-  vector<int> intersection(vector<int>& nums1,vector<int>& nums2)
-  {
-    int i=0,j=0;
-    int m=nums1.size(),n=nums2.size();
-    vector<int> result;
-    sort(nums1.begin(),nums1.end());
-    sort(nums2.begin(),nums2.end());
-    while(i<m && j<n)
-    {
-        if(nums1[i]==nums2[j])
-          {
-            result.push_back(nums1[i]);
-             i++;
-             j++;
-          }
-        else if(nums1[i]>nums2[j])
-          j++;
-        else
-         i++; 
-        while(i>0 && i<m && nums1[i]==nums1[i-1])
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0;
+        int j=0;
+        vector<int>ans;
+        
+        while(i<nums1.size() && j<nums2.size())
+        {
+            if(nums1[i]== nums2[j])
+            {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+            while(i>0 && i<nums1.size() && nums1[i]==nums1[i-1])
           i++;
-        while(j>0 && j<n && nums2[j]==nums2[j-1])
+        while(j>0 && j<nums2.size() && nums2[j]==nums2[j-1])
           j++;   
+        }
+        return ans;
     }
-    return result;
- }
 };
