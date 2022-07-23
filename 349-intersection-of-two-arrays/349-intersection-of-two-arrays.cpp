a5a15d766a0a473f3924d23d9647a582eeb34e56
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-07-23 15:50:42 +0530

    Time: 3 ms (96.60%), Space: 10 MB (78.97%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,28 +1,30 @@
-class Solution {
-public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        
-        vector<int>ans;
-        set<int>s;
-        
-        for(int i=0; i<nums1.size(); i++)
-        {
-            
-            for(int j=0; j<nums2.size(); j++)
-            {
-                
-                if(nums1[i]==nums2[j])
-                {
-                    
-                    s.insert(nums1[i]);
-                   
-                }
-            }
-            
-        }
-         for (auto itr = s.begin(); itr != s.end(); itr++) {
-            ans.push_back(*itr);
-        }
-        return ans;
+class Solution
+{
+  public:
+  vector<int> intersection(vector<int>& nums1,vector<int>& nums2)
+  {
+    int i=0,j=0;
+    int m=nums1.size(),n=nums2.size();
+    vector<int> result;
+    sort(nums1.begin(),nums1.end());
+    sort(nums2.begin(),nums2.end());
+    while(i<m && j<n)
+    {
+        if(nums1[i]==nums2[j])
+          {
+            result.push_back(nums1[i]);
+             i++;
+             j++;
+          }
+        else if(nums1[i]>nums2[j])
+          j++;
+        else
+         i++; 
+        while(i>0 && i<m && nums1[i]==nums1[i-1])
+          i++;
+        while(j>0 && j<n && nums2[j]==nums2[j-1])
+          j++;   
     }
+    return result;
+ }
 };
