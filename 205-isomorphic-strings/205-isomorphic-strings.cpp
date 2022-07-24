81dd784579034fcec7dbbcf7d2f7e5fd0ebcce28
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-07-24 20:16:52 +0530

    Time: 15 ms (29.23%), Space: 7.1 MB (46.04%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char>m;
+       for(int i=0;i<s.size();i++){ 
+            if(m[s[i]]){
+                if(m[s[i]]!=t[i]) return false;
+            }else{
+                m[s[i]]= t[i];
+            }
+        } 
+        m.clear();
+         for(int i=0;i<s.size();i++){ 
+            if(m[t[i]]){
+                if(m[t[i]]!=s[i]) return false;
+            }else{
+                m[t[i]]= s[i];
+            }
+        }
+        return true;
+     
+        
+    }
+};
