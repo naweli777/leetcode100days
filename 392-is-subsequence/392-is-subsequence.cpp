55203b68b519fe1736d2942d3364f6455c366bfd
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-07-25 10:50:05 +0530

    Time: 7 ms (18.48%), Space: 6.3 MB (70.83%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -4,18 +4,19 @@ public:
         int i=0;
         int j=0;
         
-        while(i<s.length()){
-            if(s[i]==t[j]){
+        while(i<s.length())
+        {
+            if(s[i]==t[j])
+            {
                 i++;
                 j++;
             }else{
                 j++;;
             }
-            if(j>t.length())return false;
+            
+            if(j>t.length()) return false;
         }
-        
         return true;
         
     }
-      
 };
