d64920c4075b584a144481b701258bb258c64c32
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-08-03 12:04:57 +0530

    Time: 201 ms (77.56%), Space: 59.5 MB (33.03%) - LeetHub

---
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+        int distance = INT_MAX;
+        
+        int index =-1;
+        for(int i=0; i<points.size(); i++)
+        {
+            if(points[i][0]==x || points[i][1]==y)
+            {
+                if(abs(points[i][0]-x) + abs(points[i][1]-y) <distance)
+                {
+                    distance=abs(points[i][0]-x) + abs(points[i][1]-y);
+                    
+                    index=i;
+                }
+            }
+        }
+        return index;
+        
+    }
+};
