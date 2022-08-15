ca461f9bb99721f7a90dd729ea8b9fd943c68154
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-08-15 17:10:06 +0530

    Time: 3 ms (39.24%), Space: 6.5 MB (32.60%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         
-        vector<vector<int>> dummy( numRows);
+        vector<vector<int>>dummy(numRows);
         
-        for(int i=0; i<numRows;  i++)
-        {
-            dummy[i].resize(i+1);
-            dummy[i][0] = dummy[i][i]=1;
-            
-            for(int j=1; j<i; j++)
-            {
-                dummy[i][j]=dummy[i-1][j-1] + dummy[i-1][j];
-            }
-        }
+      for(int i=0; i<numRows; i++)
+      {
+           dummy[i].resize(i+1);
+          dummy[i][0]=dummy[i][i]=1;
+          
+          
+          for(int j=1; j<i; j++)
+          {
+              dummy[i][j] = dummy[i-1][j-1] + dummy[i-1][j];
+          }
+          
+      }
         return dummy;
+        
     }
 };
