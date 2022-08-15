bdf9a240910638cc61ed7fbf31e44c549dd2e413
Naweli Verma <64765789+naweli777@users.noreply.github.com>
2022-08-15 11:37:17 +0530

    Time: 8 ms (63.02%), Space: 10.2 MB (84.18%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        
+        
+        double product =1;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            product *= nums[i];
+        }
+        if(product > 1)
+        {
+            return 1;
+        }
+        else if(product <(1&0))
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
